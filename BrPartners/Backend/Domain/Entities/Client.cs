fa08b60e7999fa27763bcfa4b555e76156e9c34b
ItsVasconcellos <fernando.asv@uol.com.br>
2024-10-01 14:40:47 -0300

    feat: entity client + db

---
+++ BrPartners/Backend/Domain/Entities/Client.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Domain.Entities
+{
+    public class Client
+    {
+        public required string? name { get; set; }
+        public required string? email { get; set; }
+        public string? sex { get; set; }
+        public string? phone { get; set; }
+        public int? age { get; set; }
+        public DateTime? created_at { get; set; }
+    }
+}
