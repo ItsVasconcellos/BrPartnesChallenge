b5a39624d93d36f3594807e2a6aa47384f38c53c
ItsVasconcellos <fernando.asv@uol.com.br>
2024-10-01 22:12:38 -0300

    feat: clientService Interface

---
+++ BrPartners/Backend/Services/ClientService.cs
@@ -0,0 +1,41 @@
+﻿using Backend.Infra.Repositories.Interfaces;
+using Backend.Domain.Entities;
+using Backend.Services.Interfaces;
+namespace Backend.Services
+{
+    public class ClientService : IClientService
+    {
+        private readonly IClientRepository _clientRepository;
+
+        public ClientService(IClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+
+        public async Task<Client?> GetClientById(int clientId)
+        {
+            return await _clientRepository.GetClientById(clientId);
+        }
+
+        public async Task<IEnumerable<Client>> GetAllClients()
+        {
+            return await _clientRepository.GetAllClients();
+        }
+
+        public async Task<Client> CreateClient(Client client)
+        {
+            await _clientRepository.AddClient(client);
+            return client;
+        }
+
+        public async Task<bool> UpdateClient(Client client)
+        {
+            return await _clientRepository.UpdateClient(client);
+        }
+
+        public async Task<bool> DeleteClient(Client client)
+        {
+            return await _clientRepository.Delete(client);
+        }
+    }
+}
---
+++ BrPartners/Backend/Services/Interfaces/IClientService.cs
@@ -0,0 +1,10 @@
+﻿using Backend.Domain.Entities;
+
+namespace Backend.Services.Interfaces
+{
+    public interface IClientService
+    {
+        Task<Client?> GetClientById(int clientId);
+        Task<IEnumerable<Client>> GetAllClients();
+    }
+}
