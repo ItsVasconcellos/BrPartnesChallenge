b237248f6ccfd97db9d3320347deba506c273d7c
ItsVasconcellos <fernando.asv@uol.com.br>
2024-10-01 22:50:05 -0300

    feat: Client and Address DTO

---
+++ BrPartners/Backend/Domain/DTOs/Responses/AddressDTO.cs
@@ -0,0 +1,14 @@
+﻿using Backend.Domain.Enum;
+
+namespace Backend.Domain.DTOs.Responses
+{
+    public class AddressDTO
+    {
+        public required string Street { get; set; }
+        public required string City { get; set; }
+        public required string State { get; set; }
+        public required string ZipCode { get; set; }
+        public AddressType Type { get; set; }  
+    }
+
+}
---
+++ BrPartners/Backend/Domain/DTOs/Responses/ClientDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace Backend.Domain.DTOs.Responses
+{
+    public class ClientDTO
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? Sex { get; set; }
+        public string? Phone { get; set; }
+        public int? Age { get; set; }
+        public DateTime? CreatedAt { get; set; }
+
+        // Lista de endereços associados ao cliente
+        public List<AddressDTO> Addresses { get; set; } = new();
+    }
+
+}
