5c9e2a176edb9fdfe7ed40cdd7dc781d8f9d7fc9
ItsVasconcellos <fernando.asv@uol.com.br>
2024-10-01 22:38:03 -0300

    feat: address service

---
+++ BrPartners/Backend/Services/AddressService.cs
@@ -0,0 +1,37 @@
+﻿using Backend.Domain.Entities;
+using Backend.Infra.Repositories.Interfaces;
+using Backend.Services.Interfaces;
+
+namespace Backend.Services
+{
+    public class AddressService : IAddressService
+    {
+        private readonly IAddressRepository _addressRepository;
+
+        public AddressService(IAddressRepository addressRepository)
+        {
+            _addressRepository = addressRepository;
+        }
+
+        public async Task<IEnumerable<Address>> GetAllAddress()
+        {
+            return await _addressRepository.GetAllAddress();
+        }
+        public async Task<Address?> GetAddressById(int addressId)
+        {
+            return await _addressRepository.GetAddressById(addressId);
+        }
+        public async Task<bool> AddAddress(Address address)
+        {
+            return await _addressRepository.AddAddress(address);
+        }
+        public async Task<bool> UpdateAddress(Address address)
+        {
+            return await _addressRepository.UpdateAddress(address);
+        }
+        public async Task<bool> DeleteAddress(Address address)
+        {
+            return await _addressRepository.DeleteAddress(address);
+        }
+    }
+}
---
+++ BrPartners/Backend/Services/Interfaces/IAddressService.cs
@@ -0,0 +1,12 @@
+﻿using Backend.Domain.Entities;
+namespace Backend.Services.Interfaces
+{
+    public interface IAddressService
+    {
+        Task<IEnumerable<Address>> GetAllAddress();
+        Task<Address?> GetAddressById(int addressId);
+        Task<bool> AddAddress(Address address);
+        Task<bool> UpdateAddress(Address address);
+        Task<bool> DeleteAddress(Address address);
+    }
+}
