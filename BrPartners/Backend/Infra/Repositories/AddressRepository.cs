d80023d32c3af02203d4f605b4098b808e545bdf
ItsVasconcellos <fernando.asv@uol.com.br>
2024-10-01 22:28:17 -0300

    feat: address repositories

---
+++ BrPartners/Backend/Infra/Repositories/AddressRepository.cs
@@ -0,0 +1,70 @@
+﻿using Backend.Domain.Entities;
+using Backend.Infra.Data;
+using Backend.Infra.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Infra.Repositories
+{
+    public class AddressRepository : IAddressRepository
+    {
+        private readonly DataContext _context;
+        public AddressRepository(DataContext context) {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Address>> GetAllAddress()
+        {
+            return await _context.Addresses.ToListAsync();
+        }
+        public async Task<Address?> GetAddressById(int addressId)
+        {
+            return await _context.Addresses.FindAsync(addressId);
+        }
+
+        public async Task<bool> AddAddress(Address address)
+        {
+            try
+            {
+                await _context.Addresses.AddAsync(address);
+                var result = await _context.SaveChangesAsync();
+                return result > 0;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> UpdateAddress(Address address)
+        {
+            try
+            {
+                _context.Addresses.Update(address);
+                var result = await _context.SaveChangesAsync();
+                return result > 0;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public async Task<bool> DeleteAddress(Address address)
+        {
+            try
+            {
+                _context.Addresses.Remove(address);
+                var result = await _context.SaveChangesAsync();
+                return result > 0;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ BrPartners/Backend/Infra/Repositories/Interfaces/IAddressRepository.cs
@@ -0,0 +1,14 @@
+﻿using Backend.Domain.Entities;
+
+namespace Backend.Infra.Repositories.Interfaces
+{
+    public interface IAddressRepository
+    {
+        Task<IEnumerable<Address>> GetAllAddress();
+        Task<Address?> GetAddressById(int addressId);
+        Task<bool> AddAddress(Address address);
+        Task<bool> UpdateAddress(Address address);
+        Task<bool> DeleteAddress(Address address);
+        Task SaveChangesAsync();
+    }
+}
