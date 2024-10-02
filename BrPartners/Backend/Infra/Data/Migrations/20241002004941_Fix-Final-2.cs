6abb3066fa2e5ca4f55fc419151de28841c692ba
ItsVasconcellos <fernando.asv@uol.com.br>
2024-10-01 22:12:18 -0300

    fix: removing wrongful column on address

---
+++ BrPartners/Backend/Infra/Data/Migrations/20241002004941_Fix-Final-2.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixFinal2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Address_Clients_Clientid",
+                table: "Address");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Address",
+                table: "Address");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "Address");
+
+            migrationBuilder.RenameTable(
+                name: "Address",
+                newName: "Addresses");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Address_Clientid",
+                table: "Addresses",
+                newName: "IX_Addresses_Clientid");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Addresses",
+                table: "Addresses",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_Clients_Clientid",
+                table: "Addresses",
+                column: "Clientid",
+                principalTable: "Clients",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_Clients_Clientid",
+                table: "Addresses");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Addresses",
+                table: "Addresses");
+
+            migrationBuilder.RenameTable(
+                name: "Addresses",
+                newName: "Address");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Addresses_Clientid",
+                table: "Address",
+                newName: "IX_Address_Clientid");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CustomerId",
+                table: "Address",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Address",
+                table: "Address",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Address_Clients_Clientid",
+                table: "Address",
+                column: "Clientid",
+                principalTable: "Clients",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
