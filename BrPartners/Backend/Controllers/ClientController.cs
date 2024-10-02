f6985f65dd1655bbf455341574b4dbc4b13b643c
ItsVasconcellos <fernando.asv@uol.com.br>
2024-10-02 00:02:04 -0300

    feat: ClientController

---
+++ BrPartners/Backend/Controllers/ClientController.cs
@@ -0,0 +1,137 @@
+﻿using Backend.Domain.Entities;
+using Backend.Infra.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Backend.Services;
+using Backend.Domain.DTOs.Responses;
+
+
+using Backend.Infra.Repositories;
+using Backend.Services.Interfaces;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ClientController : ControllerBase
+    {
+        private readonly IClientService _clientService;
+        private readonly IAddressService _addressService;
+
+
+        public ClientController(IClientService clientService, IAddressService addressService)
+        {
+            _clientService = clientService;
+            _addressService = addressService;
+        }
+
+        [HttpGet]
+        [Route("getAllClients")]
+        public async Task<ActionResult<IEnumerable<ClientDTO>>> GetClients()
+        {
+            Client[] clients = (await _clientService.GetAllClients()).ToArray();
+
+            if (clients == null || !clients.Any())
+            {
+                return NotFound();
+            }
+
+            List<ClientDTO> clientsResponse = new List<ClientDTO>();
+
+            foreach (Client client in clients) {
+                List<Address>? addresses = await _addressService.GetAddressByClientID(client.id);
+                var clientDTO = new ClientDTO
+                {
+                    Id = client.id,
+                    Name = client.name,
+                    Email = client.email,
+                    Sex = client?.sex,
+                    Phone = client?.phone,
+                    Age = client?.age,
+                    CreatedAt = client.created_at,
+                    Addresses = addresses?.Select(a => new AddressDTO
+                    {
+                        Street = a.Street,
+                        City = a.City,
+                        State = a.State,
+                        ZipCode = a.ZipCode,
+                        Type = a.Type
+                    }).ToList()
+                };
+
+                // Add the client with addresses DTO to the list
+                clientsResponse.Add(clientDTO);
+            }
+
+            // Returns 200 OK with the list of clients and their addresses
+            return Ok(clientsResponse);
+            
+        }
+
+        [HttpGet("{id}")]
+
+        [Route("getClient")]
+        public async Task<ActionResult<Client>> GetClient(int id)
+        {
+            Client client = await _clientService.GetClientById(id);
+
+            if (client == null )
+            {
+                return NotFound();
+            }
+
+            List<Address>? addresses = await _addressService.GetAddressByClientID(client.id);
+            var clientDTO = new ClientDTO
+            {
+                Id = client.id,
+                Name = client.name,
+                Email = client.email,
+                Sex = client?.sex,
+                Phone = client?.phone,
+                Age = client?.age,
+                CreatedAt = client.created_at,
+                Addresses = addresses?.Select(a => new AddressDTO
+                {
+                    Street = a.Street,
+                    City = a.City,
+                    State = a.State,
+                    ZipCode = a.ZipCode,
+                    Type = a.Type
+                }).ToList()
+            };
+
+            return Ok(clientDTO);
+        }
+
+        [HttpPut()]
+
+        [Route("updateClient")]
+        public async Task<IActionResult> UpdateClient(Client client)
+        {
+            bool update = await _clientService.UpdateClient(client);
+            return update ? Ok() : NotFound();
+        }
+
+        [HttpPost]
+        [Route("createClient")]
+        public async Task<ActionResult> CreateClient(Client client)
+        {
+            bool update = await _clientService.CreateClient(client);
+            return update ? Ok() : NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClient(int id)
+        {
+            bool update = await _clientService.DeleteClient(id);
+            return update ? Ok() : NotFound();
+        }
+
+        private bool ClientExists(int id)
+        {
+          
+        }
+    }
+
+}
